a3f05bcb14b9d379ac7257a629fd3d4b20608812
Slava Blazhev <vsblazhev@yandex.com>
2023-01-31 15:20:57 +0100

    pr 10 after review

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,29 +1,31 @@
 import React from "react";
 
- function Card(props){
-const {card,handleCardClick} = props
+function Card(props) {
+  const { card, handleCardClick } = props;
 
-return (
-    <div className="card"  >
-   {/*  <button className="card__fullscreen-button" }> */}
-      <img src={props.card.link} alt="" className="card__image" /* style={{ backgroundImage: `url(${props.card.link})` }}  */onClick={handleClick}/>
-      <button className="card__delete-button" type="button" ></button>
-    {/* </button> */}
-    <div className="card__info-container" >
-      <h2 className="card__text">{props.card.name}</h2>
-      <div className="card__like-container">
-      <button className="card__like-button" type="button"></button>
-      <span className="card__likes">{props.card.likes.length}</span>
+  return (
+    <div className="card">
+      <img
+        src={props.card.link}
+        alt={props.card.name}
+        className="card__image"
+        onClick={handleClick}
+      />
+      <button className="card__delete-button" type="button"></button>
+
+      <div className="card__info-container">
+        <h2 className="card__text">{props.card.name}</h2>
+        <div className="card__like-container">
+          <button className="card__like-button" type="button"></button>
+          <span className="card__likes">{props.card.likes.length}</span>
+        </div>
+      </div>
     </div>
-    </div>
-  </div>
-)
+  );
 
-
-function handleClick(){
-    props.handleCardClick(props.card)
+  function handleClick() {
+    props.handleCardClick(props.card);
+  }
 }
 
-}
-
-export default Card
+export default Card;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
-import logo from '../images/logo_white.svg';
-function Header(){
-    return(
-        <header className="header">
-        <img className="header__logo" src={logo} alt="Логотип" />
-      </header>
-    )
+import logo from "../images/logo_white.svg";
+function Header() {
+  return (
+    <header className="header">
+      <img className="header__logo" src={logo} alt="Логотип" />
+    </header>
+  );
 }
 
-export default Header
+export default Header;
